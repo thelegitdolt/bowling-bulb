57d0f6d89c8374af87c5b2da91ffbac527cddbf2
thelegitdolt <asphaltpotatofy@gmail.com>
2026-05-21 23:40:44 -0700

    baby's first override

---
+++ src/main/java/com/dolthhaven/bowlingbulb/core/mixin/SuspiciousStewRecipeMixin.java
@@ -0,0 +1,31 @@
+package com.dolthhaven.bowlingbulb.core.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.SuspiciousStewRecipe;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SuspiciousStewRecipe.class)
+public class SuspiciousStewRecipeMixin {
+    @WrapMethod(method = "matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z")
+    private boolean sex(CraftingContainer pInv, Level pLevel, Operation<Boolean> original) {
+        boolean red = false, brown = false, flower = false;
+        for(ItemStack stack : pInv.getItems()) {
+            if (stack.is(Items.RED_MUSHROOM) && !red) {
+                red = true;
+            } else if (stack.is(Items.BROWN_MUSHROOM) && !red) {
+                brown = true;
+            } else if (stack.is(ItemTags.SMALL_FLOWERS) && !red) {
+                flower = true;
+            } else {
+                return false;
+            }
+        }
+        return red & brown & flower;
+    }
+}
