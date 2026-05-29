f7e1d960d18811bbb7217fa789efb9472e95b158
thelegitdolt <asphaltpotatofy@gmail.com>
2026-05-28 17:28:09 -0700

    cooking pots stack to 100

--- src/main/java/com/dolthhaven/bowlingbulb/BowlingBulb.java
+++ src/main/java/com/dolthhaven/bowlingbulb/BowlingBulb.java
@@ -1,7 +1,9 @@
 package com.dolthhaven.bowlingbulb;
 
+import com.dolthhaven.bowlingbulb.core.DoltUtils;
 import com.dolthhaven.bowlingbulb.core.registry.BowlingBulbLootModifiers;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +11,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
+import vectorwing.farmersdelight.common.registry.ModItems;
 
 @SuppressWarnings("removal")
 @Mod(BowlingBulb.MOD_ID)
@@ -30,6 +33,10 @@ public class BowlingBulb {
                 if (item.craftingRemainingItem == Items.BOWL || item.craftingRemainingItem == Items.GLASS_BOTTLE) {
                     item.craftingRemainingItem = Items.AIR;
                 }
+            });
+            ModItems.COOKING_POT.get().asItem().maxStackSize = 64;
+            DoltUtils.getPotentialItem("dungeonsdelight", "monster_pot").ifPresent(pot -> {
+                pot.maxStackSize = 64;
             });
         });
     }
